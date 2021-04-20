f3a989f6922bf4fd9dfd51dbcef484d62ec0ae43
Leon <raphalen.leon@gmail.com>
2021-04-20 23:35:48 +0200

    Texture update centralization
    - Centralized the pheromone and resource texture update.

--- Assets/Scripts/TextureManager.cs
+++ Assets/Scripts/TextureManager.cs
@@ -31,128 +31,6 @@ public class TextureManager : MonoBehaviour
         return texture;
     }
 
-    private Texture2D StoreTexture()
-    {
-        Texture2D oldTexture = new Texture2D(CONST.width, CONST.height);
-
-        for (int i = 0; i < texture.width; i++)
-        {
-            for (int j = 0; j < texture.height; j++)
-            {
-                oldTexture.SetPixel(i, j, texture.GetPixel(i, j));
-            }
-        }
-
-        return oldTexture;
-    }
-
-    private void BlurTexture()
-    {
-        Texture2D oldTexture = StoreTexture();
-        Color pixelColor;
-        Color sidePixelColor;
-        int binQty;
-        float colorScaling;
-
-        for (int i = 0; i < texture.width; i++)
-        {
-            for (int j = 0; j < texture.height; j++)
-            {
-                pixelColor = oldTexture.GetPixel(i, j);
-
-                if (pixelColor != COLOR.resource)
-                {
-                    binQty = 1;
-
-                    // Use pixel below if it exists
-                    if (i != 0)
-                    {
-                        sidePixelColor = oldTexture.GetPixel(i - 1, j);
-
-                        if (sidePixelColor != COLOR.resource)
-                        {
-                            pixelColor += sidePixelColor;
-                            binQty++;
-                        }
-
-                        // Use pixel on top if it exists
-                        if (i != (texture.height - 1))
-                        {
-                            sidePixelColor = oldTexture.GetPixel(i + 1, j);
-
-                            if (sidePixelColor != COLOR.resource)
-                            {
-                                pixelColor += sidePixelColor;
-                                binQty++;
-                            }
-                        }
-                    }
-
-                    // Use pixel to the left if it exists
-                    if (j != 0)
-                    {
-                        sidePixelColor = oldTexture.GetPixel(i, j - 1);
-
-                        if (sidePixelColor != COLOR.resource)
-                        {
-                            pixelColor += sidePixelColor;
-                            binQty++;
-                        }
-
-                        // Use pixel right if it exists
-                        if (j != (texture.width - 1))
-                        {
-                            sidePixelColor = oldTexture.GetPixel(i, j + 1);
-
-                            if (sidePixelColor != COLOR.resource)
-                            {
-                                pixelColor += sidePixelColor;
-                                binQty++;
-                            }
-                        }
-                    }
-
-                    // Compute scaling factor
-                    colorScaling = CONST.bluringFactor / ((float) binQty);
-
-                    // Mean and decay pixel color
-                    pixelColor.r *= colorScaling;
-                    pixelColor.g *= colorScaling;
-                    pixelColor.b *= colorScaling;
-                }
-
-                // Set pixel
-                texture.SetPixel(i, j, pixelColor);
-            }
-        }
-    }
-
-    private void DrawPheromones()
-    {
-        Texture2D pheromoneTexture = antManager.GetPheromoneTexture();
-
-        for (int i = 0; i < CONST.width; i++)
-        {
-            for (int j = 0; j < CONST.height; j++)
-            {
-                texture.SetPixel(i, j, pheromoneTexture.GetPixel(i, j));
-            }
-        }
-    }
-
-    private void DrawResources()
-    {
-        Texture2D resourcesTexture = resourceManager.GetResourcesTexture();
-
-        for (int i = 0; i < CONST.width; i++)
-        {
-            for (int j = 0; j < CONST.height; j++)
-            {
-                texture.SetPixel(i, j, texture.GetPixel(i, j) + resourcesTexture.GetPixel(i, j));
-            }
-        }
-    }
-
     private void DrawAnts()
     {
         List<Vector2> antsCoordinates = antManager.GetAntsCoordinates();
@@ -172,11 +50,17 @@ public class TextureManager : MonoBehaviour
 
     public void UpdateTexture()
     {
-        // Draw pheromones
-        DrawPheromones();
+        Texture2D pheromoneTexture = antManager.GetPheromoneTexture();
+        Texture2D resourcesTexture = resourceManager.GetResourcesTexture();
 
-        // Draw resources
-        DrawResources();
+        for (int i = 0; i < CONST.width; i++)
+        {
+            for (int j = 0; j < CONST.height; j++)
+            {
+                // Draw pheromones, then resources on top of them
+                texture.SetPixel(i, j, pheromoneTexture.GetPixel(i, j) + resourcesTexture.GetPixel(i, j));
+            }
+        }
 
         // Draw ants
         DrawAnts();
